13c06926eef6054145c15b4a68472f8a06314626
Miguel Angel Quintanilla <miguel.angel.quintanilla@darwinex.com>
2023-09-28 11:58:58 +0200

    [release/3.0] Refactor way of call function

--- src/main/java/com/github/migangqui/spring/localhost/run/LocalhostRunExecutor.java
+++ src/main/java/com/github/migangqui/spring/localhost/run/LocalhostRunExecutor.java
@@ -1,5 +1,11 @@
 package com.github.migangqui.spring.localhost.run;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,45 +14,39 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.concurrent.atomic.AtomicReference;
-
 @Component
 @ConditionalOnProperty(name = "localhost-run.enabled", havingValue = "true")
 public class LocalhostRunExecutor {
 
-    private final Logger log = LoggerFactory.getLogger(LocalhostRunExecutor.class);
+	private final Logger log = LoggerFactory.getLogger(LocalhostRunExecutor.class);
 
-    public LocalhostRunExecutor() {}
+	public LocalhostRunExecutor() {
+	}
 
-    @Value("${server.port:8080}")
-    private String serverPort;
+	@Value("${server.port:8080}")
+	private String serverPort;
 
-    @EventListener(ApplicationStartedEvent.class)
-    public String run() throws IOException {
-        final Process process = Runtime.getRuntime().exec(String.format("ssh -R 80:localhost:%s localhost.run", serverPort));
+	@Value("${localhost-run.subdomain:lhr.life}")
+	private String subDomain;
 
-        final BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	@EventListener(ApplicationStartedEvent.class)
+	public void execute() throws IOException {
+		final Process process = Runtime.getRuntime().exec(String.format("ssh -R 80:localhost:%s nokey@localhost.run", serverPort));
 
-        final AtomicReference<String> domain = new AtomicReference<>();
+		final Pattern urlPattern = Pattern.compile("https:\\/\\/.*." + subDomain);
 
-        String line;
-        
-        while ((line = stdInput.readLine()) != null) {
-            if (line.contains("tunneled with tls termination")) {
-                domain.set(line.split(",")[1].trim());
-                break;
-            }
-        }
+		new BufferedReader(new InputStreamReader(process.getInputStream())).lines()
+				.filter(line -> urlPattern.matcher(line).find())
+				.map(filteredLine -> findText(filteredLine, urlPattern)).findFirst()
+				.ifPresentOrElse(
+						url -> log.info("Remote access to application with url {}", url),
+						() -> log.info("Remote access to application is not available"));
+	}
 
-        if (domain.get() != null) {
-            log.info("Remote access to application with url {}", domain.get());
-        } else {
-            log.info("Remote access to application is not available");
-        }
+	private static String findText(final String filteredLine, final Pattern urlPattern) {
+		final Matcher matcher = urlPattern.matcher(filteredLine);
+		matcher.find();
+		return matcher.group(0);
+	}
 
-        return domain.get();
-    }
 }
